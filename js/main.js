e9974c6515175d4a8eba71fe33b5ae316fbce25a
mastyx <mastyx.house@gmail.com>
2024-06-13 12:18:40 +0200

    L app para funzionare manca la memorizzazione in localStorage
    -   Al momento non salva nulla in local storage pero funziona :
    +   le note si creano e si eliminano correttamente
    +   si ridimensionano in modo corretto ridimensionando il contenuto
    -   controllare la parte bassa della nota rispetto al inputBox

--- js/main.js
+++ js/main.js
@@ -1,6 +1,69 @@
 // js code
+const noteContainer = document.querySelector(".notes-container");
 
 const newNote = document.getElementById("add-note");
+
+
+newNote.addEventListener("click", ()=>{
+	console.log("crea nuova nota");
+	let inputBox = document.createElement("div");
+	inputBox.className = "input-box"
+	let note = document.createElement("div");
+	note.id = "note";
+	let cancella = document.createElement("div");
+	cancella.id = "cancella";
+	let img = document.createElement("img");
+	img.src = "../trash.png";
+
+	note.setAttribute('contentEditable', "true");
+	noteContainer.appendChild(inputBox);
+	inputBox.appendChild(note);
+	inputBox.appendChild(cancella);
+	cancella.appendChild(img);
+
+	
+
+	note.addEventListener("click", ()=>{
+		note.focus();
+	});
+
+
+	//noteContainer.addEventListener("mousedown", (event)=>{
+	//	if (event.target.classList.contains("input-box")) {
+	//		$(event.target).draggable();
+	//	}
+	//});
+		
+	$(inputBox).draggable();
+
+	const originalWidth = $(inputBox).width();
+	const originalHeight = $(inputBox).height();
+	
+	$(inputBox).resizable({
+		resize : (event, ui)=>{
+			let scaleW = ui.size.width / originalWidth;
+			let scaleH = ui.size.height / originalHeight;
+
+			$(note).css("transform", "scale(" + scaleW + "," + scaleH + ")");
+		},
+		stop : (event, ui)=>{
+			$(note).css("transform","scale(1 ,1)");
+			$(note).width(ui.size.widht);
+			$(note).height(ui.size.height);
+		},
+	});
+
+	console.log(originalWidth);
+
+	
+	// elimina nota 
+	cancella.addEventListener("click", (event)=>{
+		event.stopPropagation();	
+		event.target.parentElement.parentElement.remove();
+	});
+});
+
+
 
 
 
