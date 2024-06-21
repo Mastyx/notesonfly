5a8ca22ed7021f848873c0b23888c04011b322f7
mastyx <mastyx.house@gmail.com>
2024-06-21 10:48:52 +0200

    Abbiamo completato ....
    tolta la parte che doveva gestire le linne di unione perche non
    funzionavano quando venivano cancellata
    La gestione delle linee di collegamente era con LeaderLine js

--- js/main.js
+++ js/main.js
@@ -1,152 +1,65 @@
-//// js code
-//const noteContainer = document.querySelector(".notes-container");
-//const newNote = document.getElementById("add-note");
-//const btnGroup = document.getElementById("group");
-//
-//const showNotes = ()=> {
-//	noteContainer.innerHTML = localStorage.getItem("notes");
-//}
-//
-//const updateStorage = ()=>{
-//	localStorage.setItem("notes", noteContainer.innerHTML);
-//}
-//
-//newNote.addEventListener("click", ()=>{
-//	console.log("crea nuova nota");
-//	let inputBox = document.createElement("div");
-//	inputBox.className = "input-box";
-//	let note = document.createElement("textarea");
-//	note.id = "note";
-//	let cancella = document.createElement("div");
-//	cancella.id = "cancella";
-//	let img = document.createElement("img");
-//	img.src = "../trash.png";
-//	// note.setAttribute('contentEditable', "true");
-//	noteContainer.appendChild(inputBox);
-//	inputBox.appendChild(note);
-//	inputBox.appendChild(cancella);
-//	cancella.appendChild(img);
-//	note.addEventListener("click", ()=>{
-//		note.focus();
-//	});
-//	// gestisci tab
-//	note.addEventListener("keydown", (event)=>{
-//		const start = note.selectionStart;
-//		const end = note.selectionEnd;
-//		const tabSpace = "    ";
-//		// gestisce il tab
-//		if (event.key === "Tab" || event.code === "Tab") {
-//			event.preventDefault();
-//			console.log("tab press...");
-//			note.value = note.value.substring(0, start) + tabSpace + note.value.substring(end);
-//			note.selectionStart = note.selectionEnd = start + 4;
-//		}
-//		// gestisce l'identazione
-//		if ( event.key === 'Enter' || event.code === 'Enter' ) {
-//			event.preventDefault();
-//			const lineStart = note.value.lastIndexOf('\n', start - 1) + 1;
-//			const lineText = note.value.substring(lineStart, start);
-//			const leadingTabs = lineText.match(/^\s*/)[0];
-//
-//			note.value = note.value.substring(0, start) + '\n' +
-//				leadingTabs + note.value.substring(end);
-//			note.selectionStart = note.selectionEnd = start + 1 + leadingTabs.length;
-//		}
-//		
-//	});
-//
-//	$(inputBox).draggable({});
-//	
-//	const originalWidth = $(inputBox).width();
-//	const originalHeight = $(inputBox).height();
-//	
-//	$(inputBox).resizable({
-//		// durante il resize 
-//		resize : (event, ui)=>{
-//			let scaleW = ui.size.width / originalWidth;
-//			let scaleH = ui.size.height / originalHeight;
-//			$(note).css("transform", "scale(" + scaleW + "," + scaleH + ")");
-//		},
-//		// alla fine 
-//		stop : (event, ui)=>{
-//			$(note).css("transform","scale(1 ,1)");
-//			$(note).width(ui.size.widht);
-//			$(note).height(ui.size.height);
-//			$(note).css("height", "90%");
-//			$(note).css("width", "100%");
-//
-//
-//		},
-//	});
-//
-//	console.log(originalWidth);
-//
-//	
-//	// elimina nota 
-//	cancella.addEventListener("click", (event)=>{
-//		event.stopPropagation();
-//		let confirmed = confirm("Are you sure ? ");
-//		if (confirmed) {
-//			event.target.parentElement.parentElement.remove();}
-//	});
-//});
-//
-//
-//// raggruppa note
-//const groupNote = ()=> {
-//	const notes = document.querySelectorAll(".input-box");
-//	let topPosition = 100;
-//	notes.forEach( nota => {
-//		nota.style.position = 'relative';
-//		nota.style.left = '50px';=
-//		nota.style.top = topPosition + "px";
-//	});
-//}
-//
-//group.addEventListener('click', ()=>{
-//	groupNote();
-//});
 
-// proviamo l'approccio con le classi
+
 document.addEventListener("DOMContentLoaded", ()=> {
+	
 	const noteContainer = document.querySelector(".notes-container");
 	const newNoteButton = document.getElementById("add-note");
-	let currentFontSize = 16 ;
+	const resetBtn = document.getElementById("reset-btn");	
 	let notes = [];
-	let zIndexCount = 1; 
+	let zIndexCount = 1;
 
+	
 	class Nota {
 		constructor(content, position, size, fontsize) {
 			this.id = 'note-'+ new Date().getTime(); 
 			this.content = content || "";
 			this.position = position || { top : 100, left : 10};
-			this.size = size || { width : 200, height : 200 };
-			this.fontSize = fontsize || 16;
+			this.size = size || { width : 400, height : 300 };
+			this.fontSize = fontsize || 16 ;
 			this.createElement();
 		}
-
+		// il costruttore richiama la nota
 		createElement() {
+			// div cancella contenente l'immagine cestino 
+			this.cancella = document.createElement("div");
+			this.cancella.id = "cancella";
+			let img = document.createElement("img");
+			img.src = "../trash.png";
+
 			this.inputBox = document.createElement("div");
 			this.inputBox.className = "input-box";
 			this.inputBox.style.top = this.position.top + "px";
 			this.inputBox.style.left = this.position.left + "px";
 			this.inputBox.style.width = this.width + "px";
 			this.inputBox.style.height = this.height + "px";
-
+			this.inputBox.style.zIndex = ++zIndexCount;
+			// elemento nota 
 			this.note = document.createElement("textarea");
-			this.note.id = this.id;
+			this.note.id = this.id; 
 			this.note.style.fontSize = this.fontSize + "px";
 			this.note.value = this.content;
-			
-			this.cancella = document.createElement("div");
-			this.cancella.id = "cancella";
-			let img = document.createElement("img");
-			img.src = "../trash.png";
-			
+
+			// gestione size testo
+			this.containerBtn = document.createElement("div");
+			this.containerBtn.id = "containerBtn";
+		
+			this.increaseFont = document.createElement("button");
+			this.increaseFont.id = "btnIncreaseFont";
+			this.increaseFont.innerText = "+";
+
+			this.decreaseFont = document.createElement("button");
+			this.decreaseFont.id = "btnDecreaseFont";
+			this.decreaseFont.innerText = "-";
+
+		
 			// colleghiamo il tutto 
 			this.cancella.appendChild(img);
+			this.containerBtn.appendChild(this.increaseFont);
+			this.containerBtn.appendChild(this.decreaseFont);
+			this.containerBtn.appendChild(this.cancella);
+
 			this.inputBox.appendChild(this.note);
-			this.inputBox.appendChild(this.cancella);
+			this.inputBox.appendChild(this.containerBtn);
 			noteContainer.appendChild(this.inputBox);
 			
 			// ascoltatore
@@ -157,6 +70,15 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
 		// create function
 		addEventListener() {
+			this.increaseFont.addEventListener("click", ()=>{
+				++this.fontSize;
+				this.updateFontSize(this.fontSize);	
+			});
+			this.decreaseFont.addEventListener("click", ()=>{
+				--this.fontSize;
+				this.updateFontSize(this.fontSize);	
+			});
+
 			this.cancella.addEventListener("click", (event)=> {
 				event.stopPropagation();
 				console.log("Cancella");
@@ -175,6 +97,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
 			});
 
 			this.note.addEventListener('click', ()=>{
+				// aggiungio il focus sulla nota(textarea) 
+				// altrimenti su dis mobile non fa scrivere 
 				this.note.focus();
 			});
 	
@@ -183,16 +107,44 @@ document.addEventListener("DOMContentLoaded", ()=> {
 				this.content = this.note.value;	
 				saveNotes();
 			});
-
+			// disable drag quando tocco la textarea (note)
 			this.note.addEventListener("touchstart", (event)=>{
 				this.inputBox.draggable("disable");
 			});
+			// riabilito il drag 
 			this.note.addEventListener("touchend", (event)=>{
 				this.inputBox.draggable("enable");
 			});
-		
 
+			// gestione del Tab e identazione
+			this.note.addEventListener("keydown", (event)=>{
+				const start = this.note.selectionStart;
+				const end = this.note.selectionEnd;
+				const tabspace = "   ";
+				if (event.key === 'Tab' || event.code === 'Tab') {
+					event.preventDefault();
+					console.log('tab');
+					this.note.value = 
+						this.note.value.substring(0, start) + 
+							tabspace + this.note.value.substring(end);
+					this.note.selectionStart = this.note.selectionEnd = start + 3;
+				}
+				// gestire identazione quando press enter
+				if (event.key === "Enter" || event.code === "Enter") {
+					event.preventDefault();
+					const lineStart = this.note.value.lastIndexOf('\n', start - 1)+1;
+					const lineText = this.note.value.substring(lineStart, start);
+					const leadingTabs = lineText.match(/^\s*/)[0];
+					this.note.value = this.note.value.substring(0, start) + '\n' +
+						leadingTabs + this.note.value.substring(end);
+					this.note.selectionStart = this.note.selectionEnd = start + 1 + leadingTabs.length;
+					setTimeout(()=>{
+						this.note.scrollTop = this.note.scrollHeight;
+					}, 0 );
+				}
+			});	
 		}
+
 		// gestisce il trascinamento
 		makeResizableAndDraggable() {
 			const originalWidth = $(this.inputBox).width();
@@ -200,6 +152,8 @@ document.addEventListener("DOMContentLoaded", ()=> {
 			
 			$(this.inputBox).draggable({
 				containment : '.note-container',
+				drag : ()=>{
+				},
 				stop : ()=> {
 					this.position = {
 						top : this.inputBox.offsetTop,
@@ -211,15 +165,10 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
 			$(this.inputBox).resizable({
 				resize : (event, ui) => {
-					let scaleW = ui.size.width / originalWidth;
-					let scaleH = ui.size.height / originalHeight;
-					$(this.note).css("transform", "scale("+ scaleW +","+ scaleH +")");
 				},
 				stop: (event, ui) => {
-					$(this.note).css("transform", "scale(1,1)");
-					this.note.style.width = ui.size.width + "px";
-					this.note.style.height =  `100%-${this.cancella.style.height}`;
-					console.log(this.cancella.style.height);
+					this.note.style.width = "100%";
+					this.note.style.height = "100%";
 					this.size = { width : ui.size.widht , height : ui.size.height };
 					saveNotes();
 				},
@@ -230,14 +179,35 @@ document.addEventListener("DOMContentLoaded", ()=> {
 		updateFontSize(size) {
 			this.fontSize = size;
 			this.note.style.fontSize = size + "px";
+			saveNotes();
 		}
+
+		//reset position e resize all notes
+		resetPositionResize(index) {
+			const originalWidth = 400;
+			const originalHeight = 400;
+			const margin = 10;
+			const top = 100 + (originalHeight + margin) * index;
+
+			this.inputBox.style.width = originalWidth + 'px';
+			this.inputBox.style.height = originalHeight + 'px';
+			this.inputBox.style.top = top + 'px';
+			this.inputBox.style.left = "10px";
+
+			this.note.style.width = "100%";
+			this.note.style.height = `100%`;
+
+			this.size = {with: originalWidth, height : originalHeight };
+			this.position = {top: top, left: 10};
+			saveNotes();
+		}
+		 
 	}
-
-
-
+	// -----------end class---------------------------------------------
+	
 	newNoteButton.addEventListener("click", ()=> {
 
-		let nota = new Nota("", {top: 100 + notes.length*5 , left: 10+ notes.length*5}, {width: 200, height: 200});
+		let nota = new Nota("", {top: 100 + notes.length*5 , left: 10+ notes.length*5}, {width: 400, height: 300} );
 		notes.push(nota);
 		saveNotes();
 	});
@@ -262,155 +232,21 @@ document.addEventListener("DOMContentLoaded", ()=> {
 		}
 	}
 
-	loadNotes(); // richiama la funzione per caricare le note
+	const resetResizePos = ()=>{
+		notes.forEach( (nota, index)=> {
+			nota.resetPositionResize(index);
+		});
+	}
+	resetBtn.addEventListener("click", ()=>{
+		resetResizePos();
+	})
+
+
+	loadNotes();
+	// richiama la funzione per caricare le note
+
 
 });
 
 
 
-
-// sto modificando tutto per utilizzare un approccio object oriented
-// creando la classe Nota questo e il codice di chat groupNote
-//
-//				-- -- -- -- -- -- -- -- -- -- -- -- 
-//
-//document.addEventListener("DOMContentLoaded", () => {
-//  const noteContainer = document.querySelector(".notes-container");
-//  const newNoteButton = document.getElementById("add-note");
-//  let currentFontSize = 16;
-//  let notes = []; // Array per tracciare le note create
-//
-//  class Nota {
-//    constructor(content, position, size, fontSize) {
-//      this.content = content || ""; // Assicura che this.content sia sempre una stringa
-//      this.position = position || { top: 10, left: 10 };
-//      this.size = size || { width: 200, height: 200 };
-//      this.fontSize = fontSize || 16;
-//
-//      this.createElement();
-//    }
-//
-//    createElement() {
-//      this.inputBox = document.createElement("div");
-//      this.inputBox.className = "input-box";
-//      this.inputBox.style.top = this.position.top + "px";
-//      this.inputBox.style.left = this.position.left + "px";
-//      this.inputBox.style.width = this.size.width + "px";
-//      this.inputBox.style.height = this.size.height + "px";
-//
-//      this.note = document.createElement("textarea");
-//      this.note.id = "note";
-//      this.note.style.fontSize = this.fontSize + "px";
-//      this.note.value = this.content;
-//
-//      this.cancella = document.createElement("div");
-//      this.cancella.id = "cancella";
-//      let img = document.createElement("img");
-//      img.src = "../trash.png";
-//
-//      this.cancella.appendChild(img);
-//      this.inputBox.appendChild(this.note);
-//      this.inputBox.appendChild(this.cancella);
-//      noteContainer.appendChild(this.inputBox);
-//
-//      this.addEventListeners();
-//      this.makeResizableAndDraggable();
-//    }
-//
-//    addEventListeners() {
-//      this.cancella.addEventListener("click", (event) => {
-//        event.stopPropagation();
-//        let confirmed = confirm("Sei sicuro di voler cancellare questa nota?");
-//        if (confirmed) {
-//          this.inputBox.remove();
-//          notes = notes.filter(n => n !== this);
-//          saveNotes();
-//        }
-//      });
-//
-//      this.note.addEventListener("click", () => {
-//        this.note.focus();
-//      });
-//    }
-//
-//    makeResizableAndDraggable() {
-//      const originalWidth = $(this.inputBox).width();
-//      const originalHeight = $(this.inputBox).height();
-//
-//      $(this.inputBox).draggable({
-//        containment: ".notes-container",
-//        stop: () => {
-//          this.position = {
-//            top: this.inputBox.offsetTop,
-//            left: this.inputBox.offsetLeft
-//          };
-//          saveNotes();
-//        }
-//      });
-//
-//      $(this.inputBox).resizable({
-//        resize: (event, ui) => {
-//          let scaleW = ui.size.width / originalWidth;
-//          let scaleH = ui.size.height / originalHeight;
-//
-//          $(this.note).css("transform", "scale(" + scaleW + "," + scaleH + ")");
-//        },
-//        stop: (event, ui) => {
-//          $(this.note).css("transform", "scale(1, 1)");
-//          this.note.style.width = ui.size.width + "px";
-//          this.note.style.height = ui.size.height + "px";
-//          this.size = { width: ui.size.width, height: ui.size.height };
-//          saveNotes();
-//        }
-//      });
-//    }
-//
-//    updateFontSize(size) {
-//      this.fontSize = size;
-//      this.note.style.fontSize = size + "px";
-//    }
-//  }
-//
-//  newNoteButton.addEventListener("click", () => {
-//    let nota = new Nota("", { top: 10, left: 10 }, { width: 200, height: 200 }, currentFontSize);
-//    notes.push(nota);
-//    saveNotes();
-//  });
-//
-//  document.getElementById("increase-font").addEventListener("click", () => {
-//    currentFontSize += 2;
-//    notes.forEach(nota => {
-//      nota.updateFontSize(currentFontSize);
-//    });
-//    saveNotes();
-//  });
-//
-//  document.getElementById("decrease-font").addEventListener("click", () => {
-//    currentFontSize -= 2;
-//    notes.forEach(nota => {
-//      nota.updateFontSize(currentFontSize);
-//    });
-//    saveNotes();
-//  });
-//
-//  function saveNotes() {
-//    const noteData = notes.map(nota => ({
-//      content: nota.note.value,
-//      position: nota.position,
-//      size: nota.size,
-//      fontSize: nota.fontSize
-//    }));
-//    localStorage.setItem("notes", JSON.stringify(noteData));
-//  }
-//
-//  function loadNotes() {
-//    const noteData = JSON.parse(localStorage.getItem("notes"));
-//    if (noteData) {
-//      notes = noteData.map(data => new Nota(data.content, data.position, data.size, data.fontSize));
-//    }
-//  }
-//
-//  loadNotes();
-//});
-//
-//
